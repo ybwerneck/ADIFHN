d127d76b3a0609c208d56e74ce13ae3a14c9c3ec
Yan <yan@bwerneck@outlook.com>
2020-07-28 04:04:32 -0300

    deletando pastas antingas

--- adi/calcerro/adifhn.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef ADIFHN_H
-#define ADIFHN_H
-void adifhn(double,double, double, char[],bool report);
-
-#endif // ADIFHN_H
-
--- adi/errorcal/adifhn.h
+++
@@ -1,5 +0,0 @@
-#ifndef ADIFHN_H
-#define ADIFHN_H
- void adifhn(int , int, char *);
-
-#endif // ADIFHN_H
